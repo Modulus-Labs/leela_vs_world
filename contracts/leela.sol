cee3f86151143b8c393e6dd7e4279de323201364
Allison Qi <aqi@princeton.edu>
2022-12-28 12:52:41 -0800

    add boilerplate leela

---
+++ contracts/leela.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.7.6;
+import "../libraries/SafeMath.sol";
+import "../libraries/Math.sol";
+import "./betting.sol";
+import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
+// SPDX-License-Identifier: UNLICENSED
+// emit the play move events and the start game events
+// betting will emit the end game events
+//optional: emit check events
+
+contract Leela {
+    function initializeLeela(){
+        return;
+    }
+
+    function updateLeela(){
+        return;
+    }
+}
