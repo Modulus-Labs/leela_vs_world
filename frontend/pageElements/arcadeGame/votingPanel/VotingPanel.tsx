7be56d9d9ffc2efe46ca738d3a9a2ed2e90d8b89
TharunTK <tharun.tiruppali@gmail.com>
2023-02-02 14:27:30 -0500

    upd

--- frontend/pageElements/arcadeGame/votingPanel/VotingPanel.tsx
+++ frontend/pageElements/arcadeGame/votingPanel/VotingPanel.tsx
@@ -4,11 +4,12 @@ import clsx from 'clsx';
 import { RetroButton } from './RetroButton';
 import { RetroDropdown } from './RetroDropdown';
 import { useBettingContext } from '../../../contexts/BettingContext';
-import { CHESS_PLAYER, MovedBoardState, MOVE_STATE } from '../../../types/Chess.type';
+import { BoardState, CHESS_PLAYER, MovedBoardState, MOVE_STATE } from '../../../types/Chess.type';
 import { useChessGameContext } from '../../../contexts/ChessGameContext';
-import { Square } from 'chess.js';
+import { Move, Square, Chess } from 'chess.js';
 
-const getAlgebraicNotation = (moveFrom: string | null, moveTo: string| null, fen: string) => {
+const getAlgebraicNotation = (currChessBoard: BoardState, moveFrom: string | null, moveTo: string| null, fen: string) => {
+  //function to replace number in fen with empty space
   const convertFenWithEmpty = () => {
     let pieces = fen.split(' ')[0];
     for(let i = 0; i<pieces.length; i++) {
@@ -26,19 +27,25 @@ const getAlgebraicNotation = (moveFrom: string | null, moveTo: string| null, fen
     }
     return pieces.split('/');
   };
+  //function that gets the chess position based on the ith, jth square it is in
   const convertIntToPosition = (i: number,j: number) => {
-    return String.fromCharCode(105-i) + j.toString();
+    return String.fromCharCode(97+j) + (8-i).toString();
   }
-  const getPositionOfPiece = (piece: string) => {
+  //function that gets all of the positions of a given piece, excluding the given one
+  const getPositionOfPiece = (piece: string, fromPos: string) => {
     const piecesArr = convertFenWithEmpty();
+    console.log(convertIntToPosition(0,0));
     let ret = [];
     for(let i = 0;i < piecesArr.length; i++) 
       for(let j = 0; j < 8; j++) 
-        if(piecesArr[i].charAt(j) == piece) 
-          ret.push(convertIntToPosition(i,j));
-    return piecesArr;
-    
+        if(piecesArr[i].charAt(j) == piece) {
+          var val = convertIntToPosition(i,j);
+          if(val != fromPos)
+            ret.push(convertIntToPosition(i,j));
+        }
+    return ret;
   }
+
   if(moveTo == null)
     return ' ';
   else if (moveFrom == null)
@@ -48,11 +55,16 @@ const getAlgebraicNotation = (moveFrom: string | null, moveTo: string| null, fen
     let fromPiece = piecesArr[8-parseInt(moveFrom.charAt(1))].charAt(moveFrom.charCodeAt(0)-97);
     if(fromPiece == "P")
       fromPiece = '';
-    console.log(fromPiece);
     let toPiece = piecesArr[8-parseInt(moveTo.charAt(1))].charAt(moveTo.charCodeAt(0)-97);
     // todo: need to deal with moves that have multiple potential fromPieces
     //pseudo code
-    const piecePositions = getPositionOfPiece(fromPiece);
+    const piecePositions = getPositionOfPiece(fromPiece, moveFrom);
+    console.log(piecePositions);
+    const chess = new Chess(currChessBoard.fen);
+    // const rawcurrGameStateidMoves: Move[] = chess.moves({
+    //   square,
+    //   verbose: true,
+    // }) as Move[];
     // remove the fromPiece. find each piece's valid moves from contract, and check if moveTo is there.
     // if moveTo is there, 
           //if pieces are not on same file 
@@ -69,7 +81,7 @@ const getAlgebraicNotation = (moveFrom: string | null, moveTo: string| null, fen
 }
 
 export const VotingPanel: FC = () => {
-  const { currChessBoard } = useChessGameContext();
+  const { currChessBoard  } = useChessGameContext();
   const {
     playerOption,
     setPlayerOption,
@@ -83,36 +95,27 @@ export const VotingPanel: FC = () => {
   return (
     <div className="relative h-full w-full bg-[url(/VotingDisplay.svg)] bg-contain bg-bottom bg-no-repeat">
       <div className="absolute left-[180px] bottom-[247.5px]">
-        <RetroDropdown
-          text={playerOption}
+      <RetroDropdown
+          text={getAlgebraicNotation(currChessBoard, currChessBoard.moveFrom, currChessBoard.moveTo, currChessBoard.fen)}
           onClick={() => {
-            setPlayerOption(
-              playerOption === CHESS_PLAYER.LEELA
-                ? CHESS_PLAYER.WORLD
-                : CHESS_PLAYER.LEELA
-            );
+            //todo: get valid moves from betting contract, and make the next item be the next valid move. 
+            // setSelectedMoveIndex((selectedMoveIndex + 1) % validMoves.length);
+            //square = new selected move
+
+            // const newChessBoard = {
+            //   ...currChessBoard,
+            //   moveState: MOVE_STATE.MOVED,
+            //   validMoves: null,
+            //   moveTo: square,
+            // } as MovedBoardState;
+        
+            // setCurrChessBoard({ ...newChessBoard });
+            // 
           }}
         />
       </div>
       <div className="absolute right-[100px] bottom-[247.5px]">
-        <RetroDropdown
-          text={getAlgebraicNotation(currChessBoard.moveFrom, currChessBoard.moveTo, currChessBoard.fen)}
-          onClick={() => {
-            //todo: get valid moves from betting contract, and make the next item be the next valid move. 
-
-            // setSelectedMoveIndex((selectedMoveIndex + 1) % validMoves.length);
-            // const endMove = (square: Square) => {
-            //   const newChessBoard = {
-            //     ...currChessBoard,
-            //     moveState: MOVE_STATE.MOVED,
-            //     validMoves: null,
-            //     moveTo: square,
-            //   } as MovedBoardState;
-          
-            //   setCurrChessBoard({ ...newChessBoard });
-            // };
-          }}
-        />
+        
       </div>
       <div className="absolute bottom-[20px] left-[0px] right-[0px] flex flex-col items-center gap-y-3">
         <div className="h-[45px] w-[545px]">
@@ -128,6 +131,7 @@ export const VotingPanel: FC = () => {
               if(moveTo)
                 moveToInt = 8*(moveTo.charCodeAt(0)-97) + parseInt(moveTo.charAt(1));
               const ret = moveFromInt<<6 + moveToInt;
+              
               // call betting contract function voteWorldMove(ret)
             }}
           />
@@ -144,6 +148,7 @@ export const VotingPanel: FC = () => {
           <RetroButton
             buttonImageUrl="bg-[url(/ConnectWalletButton.svg)]"
             onClick={() => {
+              //todo: connect the wallet from the user. 
               console.log('Connect Your Wallet');
             }}
           />
