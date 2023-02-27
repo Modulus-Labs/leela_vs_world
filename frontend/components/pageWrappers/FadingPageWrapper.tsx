7c2c665bfd16a08ca058333705895428c5b1b151
Ryan Cao <ryan@moduluslabs.xyz>
2023-02-26 18:59:38 -0800

    whoops

---
+++ frontend/components/pageWrappers/FadingPageWrapper.tsx
@@ -0,0 +1,32 @@
+import { motion, Variants } from "framer-motion";
+import { FC, ReactNode } from "react";
+
+const PageVariants: Variants = {
+  hidden: {
+    filter: "brightness(0)",
+  },
+  visible: {
+    filter: "brightness(1)",
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+type FadingPageWrapperProps = {
+  children: ReactNode;
+};
+
+export const FadingPageWrapper: FC<FadingPageWrapperProps> = ({ children }) => {
+  return (
+    <motion.div
+      variants={PageVariants}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      className="bg-off-black"
+    >
+      {children}
+    </motion.div>
+  );
+};
